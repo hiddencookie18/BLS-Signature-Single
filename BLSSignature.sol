f701cd65b4c58ea5459579da88f2ced52ca4cf4b
Ahmet Can Damci <45011270+hiddencookie18@users.noreply.github.com>
2019-05-06 19:51:32 -0400

    Update BLSSignature.sol

--- BLSSignature.sol
+++ BLSSignature.sol
@@ -1,7 +1,8 @@
 pragma solidity ^0.4.14;
 
 /* 
--Pairing and other elliptic operations are based on the code from "https://gist.github.com/BjornvdLaan/ca6dd4e3993e1ef392f363ec27fe74c4"
+-Pairing operation is based on the code from "https://github.com/Project-Arda/bgls-on-evm" 
+and curve operations are based on the code from "https://gist.github.com/BjornvdLaan/ca6dd4e3993e1ef392f363ec27fe74c4"
 
 -Test cases that provide inputs of (signature, pubkey, message) can be generated using BLS_test.py
 
@@ -34,57 +35,80 @@ library BLSSignature {
             8495653923123431417604973247489272438418190587263600148770280649306958101930]
         );
     }
+    
+       function test() internal returns (bool){
+        bytes memory message = "testmyhash";
+        
+        
+        G1Point memory signature = G1Point(12549556076515499309678673192404568282590030638915141025699763738995528297829, 10069141092585373492406241879436116983969003105301475420698830666872304665050);
+
+        G2Point memory pubkey = G2Point(
+            [10030244269781260283755623675552146909292962154180924014297942687162625408588, 13047368847798712176526920720814293572358305691215147789002472547302297622266],
+            [3914694405438683764367653986078009943559570116874831532144098124750523839045, 677545526153648417644390756566867473077828717458641435542112485698510171785]
+        );
+
+        return BLSVerify(signature,pubkey,message);
+    }
+    
 
     function BLSVerify(G1Point signature,G2Point pubkey,bytes message) internal returns (bool) {
-        G1Point[] memory G1pts = new G1Point[](2);
-        G2Point[] memory G2pts = new G2Point[](2);
 
         G1Point memory hash = hashToG1(message);
-        
-        G1pts[0] = negate(signature);
-        G1pts[1]= hash;
-        G2pts[0]=G2();
-        G2pts[1]= pubkey;
-        
-
-        return pairing(G1pts,G2pts);
+   
+        return pairing(signature,G2(),hash,pubkey);
     }
+    
+      function pairing(G1Point a, G2Point x, G1Point b, G2Point y) internal returns (bool) {
+         uint256 field_modulus = 21888242871839275222246405745257275088696311157297823662689037894645226208583;
 
-
+    uint256[12] memory input = [a.X, a.Y, x.X[0], x.X[1], x.Y[0], x.Y[1], b.X, field_modulus - b.Y, y.X[0], y.X[1], y.Y[0], y.Y[1]];
+    uint[1] memory result;
   
-    function pairing(G1Point[] p1, G2Point[] p2) internal returns (bool) {
-        require(p1.length == p2.length);
-        uint elements = p1.length;
-        uint inputSize = elements * 6;
-        uint[] memory input = new uint[](6);
-
-        for (uint i = 0; i < elements; i++)
-        {
-            input[0] = p1[i].X;
-            input[1] = p1[i].Y;
-            input[2] = p2[i].X[0];
-            input[3] = p2[i].X[1];
-            input[4] = p2[i].Y[0];
-            input[5] = p2[i].Y[1];
-        }
-
-        uint[1] memory out;
-        bool check;
+      bool check;
 
         assembly {
-            check := call(sub(gas, 2000), 8, 0, add(input, 0x20), mul(inputSize, 0x20), out, 0x20)
+            check := call(sub(gas, 2000), 8, 0, input,  0x180, result, 0x20)
 
             switch check case 0 {invalid}
         }
         require(check);
-        return out[0] != 0;
-    }
+    
+    return result[0]==1;
+  }
+    
+      function modPow(uint256 base, uint256 exponent, uint256 modulus) internal returns (uint256) {
+    uint256[6] memory input = [32,32,32,base,exponent,modulus];
+    uint256[1] memory result;
+            bool success;
 
-    function hashToG1(bytes message) internal returns (G1Point) {
-        uint256 h = uint256(sha256(message));
-        return multiply(G1(), h);
+    assembly {
+      success :=call(sub(gas, 2000), 5, 0, input, 0xc0, result, 0x20) {
+       
+      }
     }
+    return result[0];
+  }
 
+
+  function hashToG1(bytes message) internal returns (G1Point) {
+           uint256 field_modulus = 21888242871839275222246405745257275088696311157297823662689037894645226208583;
+
+              uint256 hashval = uint256(sha256(message));
+              hashval = hashval % 21888242871839275222246405745257275088696311157297823662689037894645226208583;
+      uint256 hashtest = hashval;
+
+    while (true) {
+      uint256 result = (modPow(hashtest,3,field_modulus) + 3);
+      if (modPow(result, 10944121435919637611123202872628637544348155578648911831344518947322613104291, field_modulus) == 1) {
+        uint256 py = modPow(result, 5472060717959818805561601436314318772174077789324455915672259473661306552146, field_modulus);
+          return G1Point(hashtest,py);
+        
+      } else {
+        hashtest++;
+      }
+    }
+  }
+ 
     function negate(G1Point p) internal returns (G1Point) {
         uint q = 21888242871839275222246405745257275088696311157297823662689037894645226208583;
         if (p.X == 0 && p.Y == 0)
@@ -92,7 +116,7 @@ library BLSSignature {
         return G1Point(p.X, q - (p.Y % q));
     }
 
-    function add(G1Point p1, G1Point p2) internal returns (G1Point r) {
+      function add(G1Point p1, G1Point p2) internal returns (G1Point r) {
         uint[4] memory input;
         input[0] = p1.X;
         input[1] = p1.Y;
@@ -101,12 +125,10 @@ library BLSSignature {
         bool success;
         assembly {
             success := call(sub(gas, 2000), 6, 0, input, 0xc0, r, 0x60)
-
             switch success case 0 {invalid}
         }
         require(success);
     }
-   
     function multiply(G1Point p, uint s) internal returns (G1Point r) {
         uint[3] memory input;
         input[0] = p.X;
@@ -115,7 +137,6 @@ library BLSSignature {
         bool success;
         assembly {
             success := call(sub(gas, 2000), 7, 0, input, 0x80, r, 0x60)
-
             switch success case 0 {invalid}
         }
         require(success);
